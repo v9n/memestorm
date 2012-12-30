a8031d5aa2540b7d34927aef8dbbf606713b8447
Vinh Nguyen <kureikain@gmail.com>
2012-12-30 01:53:40 -0800

    AXConfig utility class

---
+++ Meme Storm/AXConfig.h
@@ -0,0 +1,19 @@
+//
+//  AXConfig.h
+//  Meme Storm
+//
+//  Created by Vinh Nguyen on 12/30/12.
+//  Copyright (c) 2012 Vinh Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AXConfig : NSObject
+{
+    AXConfig * instance;
+    
+}
+
++ (AXConfig *) getInstance;
+- (id) getConfig;
+@end
