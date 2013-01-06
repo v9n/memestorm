085e976dd1209bf5d1d67f23cc1750dfa4a3a177
Vinh Nguyen <kureikain@gmail.com>
2013-01-06 15:53:39 -0800

    ShareKit added as submodule

---
+++ Meme Storm/AXSHKConfigurator.h
@@ -0,0 +1,13 @@
+//
+//  AXSHKConfigurator.h
+//  Meme Storm
+//
+//  Created by Vinh Nguyen on 1/6/13.
+//  Copyright (c) 2013 Vinh Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AXSHKConfigurator : NSObject
+
+@end
---
+++ Meme Storm/AXSHKConfigurator.m
@@ -0,0 +1,13 @@
+//
+//  AXSHKConfigurator.m
+//  Meme Storm
+//
+//  Created by Vinh Nguyen on 1/6/13.
+//  Copyright (c) 2013 Vinh Nguyen. All rights reserved.
+//
+
+#import "AXSHKConfigurator.h"
+
+@implementation AXSHKConfigurator
+
+@end
