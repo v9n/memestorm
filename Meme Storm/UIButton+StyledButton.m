ad0b7db0b0369b3badfa33dc784cc4c9d4125fb2
Vinh Nguyen <kureikain@gmail.com>
2013-01-10 23:54:31 -0800

    Use cateogyr to change UiBar and UiBarButtonitem

---
+++ Meme Storm/UIButton+StyledButton.m
@@ -0,0 +1,27 @@
+//
+//  UIButton+StyledButton.m
+//  Meme Storm
+//
+//  Created by Vinh Nguyen on 1/7/13.
+//  Copyright (c) 2013 Vinh Nguyen. All rights reserved.
+//
+
+#import "UIButton+StyledButton.h"
+
+@implementation UIButton (UIButton_StyledButton)
+
++ (UIButton *)styledButtonWithBackgroundImage:(UIImage *)image font:(UIFont *)font title:(NSString *)title target:(id)target selector:(SEL)selector
+{
+    CGSize textSize = [title sizeWithFont:font];
+    CGSize buttonSize = CGSizeMake(textSize.width + 20.0f, image.size.width);
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0.0f, 0.0f, buttonSize.width, buttonSize.height)];
+    [button addTarget:target action:selector forControlEvents:UIControlEventTouchUpInside];
+    [button setBackgroundImage:image forState:UIControlStateNormal];
+    [button setTitle:title forState:UIControlStateNormal];
+    [button.titleLabel setFont:font];
+    
+    return button;
+}
+
+@end
