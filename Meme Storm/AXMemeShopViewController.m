b03d40b560090dd856cd236d5ceba53414e8d82f
kureikain <kureikain@gmail.com>
2013-06-02 02:34:56 -0700

    Using change.log file

---
+++ Meme Storm/AXMemeShopViewController.m
@@ -0,0 +1,38 @@
+//
+//  AXMemeShopViewController.m
+//  Meme Storm
+//
+//  Created by Hoa Diem Nguyet on 6/2/13.
+//  Copyright (c) 2013 Vinh Nguyen. All rights reserved.
+//
+
+#import "AXMemeShopViewController.h"
+
+@interface AXMemeShopViewController ()
+
+@end
+
+@implementation AXMemeShopViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
