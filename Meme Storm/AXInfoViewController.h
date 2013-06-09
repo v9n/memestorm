b197a69335253237c64a755c5f189e437f048c0f
kureikain <kureikain@gmal.com>
2013-06-08 19:00:50 -0700

    customize meme source list

---
+++ Meme Storm/AXInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AXInfoViewController.h
+//  Meme Storm
+//
+//  Created by Hoa Diem Nguyet on 6/8/13.
+//  Copyright (c) 2013 Vinh Nguyen. All rights reserved.
+//
+
+#import "JASidePanelController.h"
+
+@interface AXInfoViewController : JASidePanelController
+
+@end
--- Meme Storm/AxcotoAppDelegate.h
+++ Meme Storm/AxcotoAppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "JASidePanelController.h"
-#import "AXSidePanelControllerViewController.h"
+#import "AXSidePanelController.h"
 
 @class AxcotoViewController;
 
